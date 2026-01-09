bf873165aedb7e3b45c028a98c5d85191160ef2f
einzig1000 <k024g0089@g.neec.ac.jp>
2026-01-09 14:16:33 +0900

    プロジェクト ファイルを追加します。

---
+++ 00-01/00-01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <Windows.h>
+
+
+
+int main()
+{
+	printf("00-01\n\n");
+	printf("hello world  ハローワールド　はろーわーるど\n");
+	return 0;
+}
---
+++ 01-01/main.cpp
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <time.h>
+#include <list>
+using namespace std;
+
+char command;
+int loop = 0;
+
+int main()
+{
+	printf("01-01\n\n");
+	srand((unsigned int)time(NULL));
+
+	std::list<const char*> list{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		//"Nishi-Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		//"Takanawa-Gateway",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shinbashi",
+		"Yurakucho",
+	};
+
+	while (loop != 1)
+	{
+		std::cout << "1970\n\n";
+		for (auto it = list.begin(); it != list.end(); ++it)
+		{
+			std::cout << *it;
+			auto next = std::next(it);
+			if (next != list.end())
+			{
+				std::cout << " - ";
+			}
+		}
+		std::cout << "\n\n";
+
+
+		for (std::list<const char*>::iterator itr = list.begin(); itr != list.end(); ++itr)
+		{
+			if (*itr == "Tabata")
+			{
+				itr = list.insert(itr, "Nishi-Nippori");
+				++itr;
+			}
+		}
+
+
+		std::cout << "2019\n\n";
+		for (auto it = list.begin(); it != list.end(); ++it)
+		{
+			std::cout << *it;
+			auto next = std::next(it);
+			if (next != list.end())
+			{
+				std::cout << " - ";
+			}
+		}
+		std::cout << "\n\n";
+
+
+		for (std::list<const char*>::iterator itr = list.begin(); itr != list.end(); ++itr)
+		{
+			if (*itr == "Tamachi")
+			{
+				itr = list.insert(itr, "Takanawa-Gateway");
+				++itr;
+			}
+		}
+
+		std::cout << "2022\n\n";
+		for (auto it = list.begin(); it != list.end(); ++it)
+		{
+			std::cout << *it;
+			auto next = std::next(it);
+			if (next != list.end())
+			{
+				std::cout << " - ";
+			}
+		}
+		std::cout << "\n\n";
+
+		printf("コンソールアプリケーションを終了する場合は数字の１を入力してください\n");
+		command = _getch();
+		if (command == '1') loop = 1;
+	}
+	return 0;
+}
