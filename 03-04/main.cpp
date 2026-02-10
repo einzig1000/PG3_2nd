4af72d17c50da36175d44fa8c750d2edd4fed70a
einzig1000 <k024g0089@g.neec.ac.jp>
2026-02-10 16:31:29 +0900

    03-04

--- 03-04/main.cpp
+++ 03-04/main.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
-
+	Circle circle;
+	circle.SetRadius(5.0f);
+	circle.Size();
+	circle.Draw();
+	Triangle triangle;
+	triangle.SetBase(4.0f);
+	triangle.SetHeight(3.0f);
+	triangle.Size();
+	triangle.Draw();
 
 	return 0;
 }
